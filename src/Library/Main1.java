2bd25a200b2c98e2cea43293220e30d123a7255f
Lokeshwariloki <128806480+Lokeshwariloki@users.noreply.github.com>
2024-08-18 17:45:01 +0530

    Create Main1.java

---
+++ src/Library/Main1.java
@@ -0,0 +1,22 @@
+import Library.Books.Books;
+import Library.Member.Members;
+import Library.Transaction.Transactions;
+public class Main {
+    public static void main(String[] args) {
+        Books book = new Books("Pride and Prejudice", "Jane Austen", "123456789", 0);
+
+        Members member = new Members("Manu", "M001");
+
+        book.displayInfo();
+        member.displayInfo();
+
+        Transactions borrowTransaction = new Transactions(book, member, "borrow");
+        borrowTransaction.process();
+        borrowTransaction.displayInfo();
+
+        Transactions returnTransaction = new Transactions(book, member, "return");
+        returnTransaction.process();
+        returnTransaction.displayInfo();
+
+    }
+}
